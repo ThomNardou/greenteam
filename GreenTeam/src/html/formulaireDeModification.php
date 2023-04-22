5f5ebfa4cb0408d53fa3ea6b73ea0b5d7b8ac4cc
ThomNardou <thomas.nardou@eduvaud.ch>
2023-04-22 12:02:57 +0200

    essayer de modifier les valeur dans la DB

--- GreenTeam/src/html/formulaireDeModification.php
+++ GreenTeam/src/html/formulaireDeModification.php
@@ -7,16 +7,17 @@
         </head>
         <body>
             <div id="container">
-                <header>
+                <!-- <header>
                         <div id="title">
                             <h1><img src="../../resources/Images/logo.png" alt="" style="height: 100px; align-self: center;"></h1>
                         </div>
-                </header>
+                </header> -->
 
                 <form method="post">
                     <div id="formulaire">
-
-                        <label>Prénom</label>
+                        <label>Nom actuel</label>
+                        <input type="text" name="nomactuel">
+                        <label>Frequence</label>
                         <input type="text" name="prenom">
                         <label>Nom</label>
                         <input type="text" name="nom">
@@ -25,6 +26,7 @@
 		        </form>
 
                 <?php
+
                     $servername = "localhost";
 
                     $username = "root";
@@ -38,13 +40,17 @@
 
                     $nom = $_POST['nom'];
                     $frequence = $_POST['prenom'];
+                    $nomactuel = $_POST['nomactuel'];
 
-                    $requete = "INSERT INTO t_typplante VALUES('" . $nom . "','" . $frequence . "')";
-                    $resultat = $conn->query($requete);
-                    if ($resultat)
-                        echo "<p>Le contact a été ajouté</p>";
+                    
+                    $requete = "UPDATE `t_typplante` SET `plaNom` = '$nom', `plaFrequece` = '$frequence' , `plaVolume` = '20.00', `plaTempsArro` = '300' WHERE `plaNom` LIKE '$nomactuel';";
+                    var_dump($requete); //Permet de voir ce que le programme envoie
+                    
+                    // $resultat = $conn->query($requete);
+                    if ($requete)
+                        echo "<p id=testPhp>Le contact a été ajouté</p>";
                     else
-                        echo "<p>Erreur</p>";
+                        echo "<p id=Test2>Erreur</p>";
                 
                 ?>
                 </div>
